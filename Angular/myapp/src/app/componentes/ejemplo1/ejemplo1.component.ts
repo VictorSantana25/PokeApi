f841872554943ea84a9bb2f34d5897996b6dad69
V.Santana <drake.santana25@outlook.es>
2024-09-02 20:32:45 -0500

    MyApp2

--- Angular/myapp/src/app/componentes/ejemplo1/ejemplo1.component.ts
+++ Angular/myapp/src/app/componentes/ejemplo1/ejemplo1.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { MaterialModule } from '../../modulos/material/material.module';
 import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-ejemplo1',
@@ -9,10 +10,17 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './ejemplo1.component.html',
   styleUrl: './ejemplo1.component.scss'
 })
-export class Ejemplo1Component {
+export class Ejemplo1Component implements OnChanges {
+  ngOnChanges(changes: SimpleChanges): void {
+    window.alert('Imagen cambiada, alerta disparada desde el componente hijo')
+  }
+  @Input() imagenDelPadre: string = '';
+  @Output() emisor = new EventEmitter<string>();
+  imagen = 'https://material.angular.io/assets/img/examples/shiba2.jpg'
 
   nombre = '';
-
+  nombres = ['Andres', 'Juan Carlos', 'Lorena', 'Verónica']
+  
   setName(){
     this.nombre = 'Darth Vader';
   }
